82976d61f98b487f2a159bf44eb71c46b7ac77bd
Verhelst Joris <megajorver@gmail.com>
2017-11-17 17:05:14 +0200

    c++ files

---
+++ HatTrick/Source/HatTrick/CharacterMovementSprint.h
@@ -0,0 +1,20 @@
+// Just don't be a dick
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CharacterMovementSprint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HATTRICK_API UCharacterMovementSprint : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ HatTrick/Source/HatTrick/HatTrick.cpp
@@ -0,0 +1,6 @@
+// Just don't be a dick
+
+#include "HatTrick.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HatTrick, "HatTrick" );
